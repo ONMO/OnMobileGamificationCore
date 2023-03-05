f804f2bd4cfaf0c42475277455b2ca843f1235ad
Naresh Babu Kommana <naresh.kommana@onmobile.com>
2023-03-05 21:02:13 +0530

    Add package file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OnMobileGamificationCore",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "OnMobileGamificationCore",
+            targets: ["OnMobileGamificationAPISDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "OnMobileGamificationAPISDK",
+            url: "https://github.com/ONMO/OnMobileGamificationCore/releases/download/v1.0.0/OnMobileGamificationAPISDK.xcframework.zip",
+            checksum: "4110bc729f1d7570bf4d24c1ed3e6832280b1566fa25c896afd44f349c9ba91f"
+        ),
+    ]
+)
